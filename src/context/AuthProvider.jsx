892b1b29731c33622155f798507ad1c65d334998
pritom352 <pritomproshad@gmail.com>
2025-06-04 21:42:32 +0600

    set onAuthStageChanged

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,14 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { AuthContext } from "./AuthContext";
 import { auth } from "../firebase/firebase.config";
 import {
   createUserWithEmailAndPassword,
   GoogleAuthProvider,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
   signInWithPopup,
   updateProfile,
 } from "firebase/auth";
 
 const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
   const provider = new GoogleAuthProvider();
   const register = (email, password) => {
     return createUserWithEmailAndPassword(auth, email, password);
@@ -19,10 +22,23 @@ const AuthProvider = ({ children }) => {
   const googleLogin = () => {
     return signInWithPopup(auth, provider);
   };
+  const login = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+  useEffect(() => {
+    const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+    return () => {
+      unSubscribe();
+    };
+  }, []);
   const authData = {
     register,
     updateUser,
     googleLogin,
+    login,
+    user,
   };
   return <AuthContext value={authData}>{children}</AuthContext>;
 };
--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -1,15 +1,45 @@
-import React from "react";
+import React, { useContext } from "react";
 import { FaGoogle } from "react-icons/fa";
-import { Link } from "react-router";
+import { Link, useNavigate } from "react-router";
+import { AuthContext } from "../../context/AuthContext";
+import Swal from "sweetalert2";
 
 const Login = () => {
+  const { login } = useContext(AuthContext);
+  const navigate = useNavigate();
+  const handleLogin = (e) => {
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    console.log(email, password);
+    login(email, password)
+      .then(() => {
+        Swal.fire({
+          position: "top-end",
+          icon: "success",
+          title: "Login successful",
+          showConfirmButton: false,
+          timer: 1500,
+        });
+        navigate("/");
+      })
+      .catch((error) => {
+        Swal.fire({
+          position: "top-end",
+          icon: "success",
+          title: error.message,
+          showConfirmButton: false,
+          timer: 1500,
+        });
+      });
+  };
   return (
     <div className="card mx-auto bg-base-100 w-full max-w-sm shrink-0 shadow-2xl mt-30">
       <h2 className=" text-3xl font-bold mx-auto  mt-3 text-blue-400  text-shadow-lg ">
         Login <span className=" text-black ">Now</span>
       </h2>
       <div className="card-body">
-        <form className="fieldset">
+        <form onSubmit={handleLogin} className="fieldset">
           <label className="label">Email</label>
 
           <input
